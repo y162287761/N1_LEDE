e327d02421791fb0a0b0833060f3da9fd71eab65
y162287761 <32408138+y162287761@users.noreply.github.com>
2021-06-07 04:43:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,11 +14,13 @@
 sed -i 's/192.168.1.1/192.168.2.2/g' package/base-files/files/bin/config_generate
 
 #Add app
-git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
-git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
-git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
+#git clone https://github.com/fw876/helloworld.git package/lean/luci-app-ssr-plus
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
+git clone https://github.com/kenzok8/openwrt-packages.git package
+git clone https://github.com/kenzok8/small.git package
 
 
 #Add Utilities 
@@ -26,4 +28,4 @@ git clone https://github.com/tuanqing/install-program package/install-program
 
 
 #Add theme
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+#git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
